03e0ed7d522b993beeed6eda7ae0a37f336496d6
Greenlight1010 <danielghershey@gmail.com>
2025-04-24 17:45:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -50,3 +50,9 @@ def scrape():
             f"{company} may have faced controversy, lawsuits or greenwashing accusations"
         ]
     })
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
